a015af6380627fafe6d18800614c9b187391b8c5
潘端端 <enjoyjavapan@163.com>
2020-08-11 21:54:42 +0800

    测试修改

--- src/main/java/com/orient/cssrc/jenkinsdemo/controller/HelloConrtoller.java
+++ src/main/java/com/orient/cssrc/jenkinsdemo/controller/HelloConrtoller.java
@@ -14,6 +14,6 @@ public class HelloConrtoller {
 
     @GetMapping("/")
     public String sayHelllo() {
-        return "hello jenkins again";
+        return "hello jenkins again again";
     }
 }
